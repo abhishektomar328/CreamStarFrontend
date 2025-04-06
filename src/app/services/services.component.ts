0a3994ae131f0ee1d467158c9b5512a3ee53a2cd
abhishektomar328 <tomarabhishektomar5@gmail.com>
2025-04-06 09:43:40 +0530

    deploy backedn

--- src/app/services/services.component.ts
+++ src/app/services/services.component.ts
@@ -1,28 +1,52 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { HttpClientModule, HttpClient } from '@angular/common/http'; // <-- Import HttpClientModule here
+import { Observable } from 'rxjs'; // For using Observables
+
 
 @Component({
   selector: 'app-services',
-  imports: [CommonModule],
+  imports: [CommonModule,HttpClientModule],
   templateUrl: './services.component.html',
   styleUrl: './services.component.css'
 })
 export class ServicesComponent {
-  services: any[] = [];
-
-  constructor(){
+  dtId:any = '67e6d0459eec4cf56e4a3d26';
+  fdId:any = '67e6d0d49eec4cf56e4a3d2a';
+  Om:any=[]
+  Jm:any=[]
+  Ls:any=[]
+  Vt:any=[]
+  Dt:any=[]
+  Fd:any=[];
+  Wp:any = [];
+  Sb:any=[]
+  services:any = []
+  constructor(private http: HttpClient){
+    
   }
-
-  uploadImage(event: any, serviceIndex: number) {
-    const file = event.target.files[0];
-    if (file) {
-      const reader = new FileReader();
-      reader.onload = (e: any) => {
-      };
-      reader.readAsDataURL(file);
-    }
+  ngOnInit(): void {
+    // Fetch categories when the component initializes
+    this.getCategories();
   }
-
-  removeImage(serviceIndex: number, imageId: number) {
+  getCategories(): void {
+    this.http.get('https://creamstarbackend.onrender.com/api/image/images').subscribe({
+      next: (data: any) => {
+        console.log(data.categories);
+        data.categories.forEach((category: any) => {
+          if (category._id === this.dtId) {
+            this.Dt = category.images; // Store Dance Troupes images
+            console.log("Dance Troupes Images:", this.Dt);
+          } else if (category._id === this.fdId) {
+            this.Fd = category.images; // Store Flower Decoration images
+            console.log("Flower Decoration Images:", this.Fd);
+          }
+        });
+      },
+      error: (error: any) => {
+        console.error("Error fetching categories:", error);
+      }
+    });
   }
 }
+  
